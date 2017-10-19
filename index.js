77b5282d9f8b05d524ba276de9fac1d7f9b6d678
hrkd <hiroki@hrkd.net>
2017-10-19 21:19:43 +0900

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const parseQuery = ()=>{
+  const queries = location.search.replace('?','').split('&');
+  let result = {}
+  queries.forEach((val)=>{
+    const keyValue = val.split('=');
+    const braket = '%5B%5D';
+    if(keyValue[0].indexOf(braket) > 0){
+      if(typeof result[keyValue[0].replace(braket,'')] === 'undefined'){
+        result[keyValue[0].replace(braket,'')] = new Array();
+      }
+      result[keyValue[0].replace(braket,'')].push(keyValue[1].replace('+',' '));
+    }else{
+      result[keyValue[0]] = keyValue[1].replace('+',' ');
+    }
+  });
+  return result;
+}
